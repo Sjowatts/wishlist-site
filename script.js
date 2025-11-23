672124347e61d04e53e14b49e1e1b052cfcd7fb8
Sjowatts <samueljamesowainwatts@outlook.com>
2025-11-23 18:55:20 +0000

    Initial upload

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
-fetch('data/items.json')
-  .then(r=>r.json())
-  .then(items=>{
-    window.allItems=items;
-    render(items);
-  });
 
-function render(items){
-  const g=document.getElementById('gallery');
-  g.innerHTML='';
-  items.forEach(i=>{
-    g.innerHTML+=`
-    <div class="item-card">
-      <img src="${i.image}" alt="${i.name}">
-      <a href="${i.link}" target="_blank">${i.name}</a>
-    </div>`;
-  });
-}
-
-document.querySelectorAll('nav button').forEach(btn=>{
-  btn.onclick=()=>{
-    const f=btn.dataset.filter;
-    if(f==="all") render(allItems);
-    else render(allItems.filter(i=>i.category===f));
-  };
+fetch('data/items.json').then(r=>r.json()).then(items=>{
+ let cat = window.CATEGORY || null;
+ const g=document.getElementById('gallery');
+ items.filter(i=>!cat || i.category===cat).forEach(i=>{
+   g.innerHTML+=`
+   <div class='item-card'>
+     <img src='${i.image}'>
+     <a href='${i.link}' target='_blank'>${i.name}</a>
+   </div>`;
+ });
 });
