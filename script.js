3b4fbd8b33fe5f19d10cb99447a6d9ac2c578cd8
Sjowatts <samueljamesowainwatts@outlook.com>
2025-11-23 19:06:28 +0000

    Updated wishlist with fixed layout and styling

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
 fetch('data/items.json').then(r=>r.json()).then(items=>{
- let cat = window.CATEGORY || null;
+ let cat=window.CATEGORY||null;
  const g=document.getElementById('gallery');
- items.filter(i=>!cat || i.category===cat).forEach(i=>{
-   g.innerHTML+=`
+ items.filter(i=>!cat||i.category===cat).forEach(i=>{
+  g.innerHTML+=`
    <div class='item-card'>
-     <img src='${i.image}'>
+     <img src='${i.image}' alt='${i.name}'>
      <a href='${i.link}' target='_blank'>${i.name}</a>
    </div>`;
  });
